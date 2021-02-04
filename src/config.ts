743f7f13599f2c4156a9bc180a3a813847fbfde3
Frank Troia <franktroia@users.noreply.github.com>
2021-02-03 20:24:40 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export default {
   SNAP_ACCOUNTS_TOKEN_URL:
     'https://accounts.snapchat.com/accounts/oauth2/token',
   SNAP_KIT_API_URL: 'https://kit.snapchat.com/v1',
-  OAUTH_SCOPE_URL_PREFIX: 'https://auth.snapchat.com/oauth2/api/',
+  OAUTH_SCOPE_URL_PREFIX: '',
 };
